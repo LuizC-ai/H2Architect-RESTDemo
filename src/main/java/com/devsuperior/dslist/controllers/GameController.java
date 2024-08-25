9ae448d5596963e21433187160fc5d7ac33ec04a
luiz.claudioda <luiz.claudioda@algartelecom.com.br>
2024-08-25 10:18:14 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dslist.controllers;
 
+
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.service.GameService;
 
@@ -16,6 +19,11 @@ public class GameController {
     @Autowired
     private GameService service;
 
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = service.findById(id);
+        return result;
+    }
     @GetMapping
     public List<GameMinDTO> findAll() {
         List<GameMinDTO> list = service.findAll();
--- src/main/java/com/devsuperior/dslist/service/GameService.java
+++ src/main/java/com/devsuperior/dslist/service/GameService.java
@@ -4,16 +4,26 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.repositories.GameRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
 
 @Service
 public class GameService {
     
     @Autowired
     private GameRepository repository;
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) { 
+        Game result = repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new GameDTO(result);
+    }
 
     public List<GameMinDTO> findAll() {
         List<Game> result = repository.findAll();
